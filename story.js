76179af11fe6c0fec0b48e869f4bf9f38cdb7712
Ali Al Dobyan <ali.aldobyan@gmail.com>
2020-09-24 13:12:45 +0300

    test

--- story.js
+++ story.js
@@ -1,2 +1,2 @@
-let name = "Ali"; let age = 25; let hobby = "meditation"
+let name = "Ali"; let age = 25; let hobby = "Sleep"
 console.log(`Mr./Mrs. ${name}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`)
